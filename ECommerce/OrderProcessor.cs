d001da499a50c4bf9ad8cfc734c68c50673c8573
KFella64 <79869527+KFella64@users.noreply.github.com>
2022-03-14 13:44:54 -0400

    Initial commit

---
+++ ECommerce/OrderProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace ECommerce
+{
+    public class OrderProcessor
+    {
+        public OrderProcessor()
+        {
+        }
+
+        public OrderSummary ProcessOrder(ShoppingCart order)
+        {
+            var numberOfItems = order.Items.Count;
+            var subTotal = order.Items.Sum(i => i.Qty * i.Price);
+            return new OrderSummary { 
+                NumberOfItems = numberOfItems,
+                SubTotal = subTotal,
+            };
+        }
+    }
+}
